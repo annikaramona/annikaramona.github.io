f4c233abbb387699985c4fd13b92a73d2d8a775b
annikaramona <annika.ramona97@gmail.com>
2023-04-08 18:26:48 -0700

    Update templates to replace default

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Annika`,
+    siteTitleAlt: `Annika Ramona`,
+    siteHeadline: `Annika Ramona - Combining the power of data and public health to drive a healthier future`,
+    siteUrl: `https://anni.info`,
+    siteDescription: `Annika Ramona's personal portfolio`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `@annikaramona `,
   },
   trailingSlash: `never`,
   plugins: [
